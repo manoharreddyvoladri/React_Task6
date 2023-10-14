2f9c07faeac139068fee600b442ea723f50ffb2f
Manohar Reddy <manireddy2306@gmail.com>
2023-10-14 17:24:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import Navbar from './components/Navbar';
+import Hero from './components/HeroSection';
+import Skills from './components/Skills';
+import Education from './components/education';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
+import ProjectDetails from "./components/ProjectDetails";
+import { darkTheme, lightTheme } from './utils/Themes.js'
+
+
+
+
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme ={darkMode ? darkTheme : lightTheme}>
+      <Router >
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skills />
+            <Education />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+          <Experience />
+
+
+          </Wrapper>
+          <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
+        </Body>
+      </Router>
+    </ThemeProvider>
+
   );
 }
 
---
+++ src/utils/Themes.js
@@ -0,0 +1,23 @@
+export const darkTheme = {
+    bg:"	#fffff2",
+    bgLight: "#7da4db",
+    primary:"#7da4db",
+    text_primary:"#0a0a0a",
+    text_secondary:"#444444",
+    card:"White",
+    card_light: 'white',
+    button:"#0a0a0a",
+    white:"#000000",
+    black:"#eeeeee",
+}
+
+export const lightTheme = {
+    bg:"#f3f6f4",
+    bgLight: "#f3f6f4",
+    primary:"#f3f6f4",
+    text_primary:"#111111",
+    text_secondary:"#444444",
+    card:"#f3f6f4",
+    button:"#f3f6f4",
+}
+//  red - #a60c0c
