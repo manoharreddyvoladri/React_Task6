c65e7b971d06848cf8127369928e80a07045cde9
Manohar Reddy Voladri <92422839+manoharreddyvoladri@users.noreply.github.com>
2023-12-31 21:43:25 +0530

    updated urls

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/voladrimanoharreddy",
+    github: "https://github.com/manoharreddyvoladri",
     resume:
       "https://drive.google.com/file/d/1GvfwV1nKmYvVj_EdB3I75M9yWztk4pYl/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/voladrimanohrreddy/",
